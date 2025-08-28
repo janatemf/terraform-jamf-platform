bdb6f3555d60b9a19b4e9657b39f2dec19018037
N. S. Bucher <nate.bucher@jamf.com>
2025-08-28 13:28:16 -0400

    Comment out computer management settings module in onboarder management configuration

--- modules/onboarder-management-macOS/main.tf
+++ modules/onboarder-management-macOS/main.tf
@@ -28,15 +28,15 @@ module "configuration-jamf-pro-categories" {
   }
 }
 
-module "configuration-jamf-pro-computer-management-settings" {
-  source                = "../configuration-jamf-pro-computer-management-settings"
-  jamfpro_instance_url  = var.jamfpro_instance_url
-  jamfpro_client_id     = var.jamfpro_client_id
-  jamfpro_client_secret = var.jamfpro_client_secret
-  providers = {
-    jamfpro.jpro = jamfpro.jpro
-  }
-}
+#module "configuration-jamf-pro-computer-management-settings" {
+#  source                = "../configuration-jamf-pro-computer-management-settings"
+#  jamfpro_instance_url  = var.jamfpro_instance_url
+#  jamfpro_client_id     = var.jamfpro_client_id
+#  jamfpro_client_secret = var.jamfpro_client_secret
+#  providers = {
+#    jamfpro.jpro = jamfpro.jpro
+#  }
+#}
 
 module "management-macOS-rosetta" {
   source                = "../management-macOS-rosetta"
